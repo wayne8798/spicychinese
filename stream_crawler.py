457766ffff6d2bddd5cd502133ca7bcfe21488aa
SudoAttack <wayne8798@gmail.com>
2014-01-14 16:24:25 -0600

    refactor codes

--- stream_crawler.py
+++ stream_crawler.py
@@ -7,31 +7,35 @@ CONSUMER_SECRET = "S2X5iS7TZPhElZl1bq01LVMJ3CPxT0xbRKUEgckk"
 ACCESS_KEY = "319176278-N7t6T1oRJhNlx0RMePKdkyKk0UK8BEgdpIpiw31Y"
 ACCESS_SECRET = "FxbhHZ5g5ddLdGFGI4TTTlwuScFoOIVUi7FZSQBxqg4DD"
 
+def analyze_user(api, screen_name):
+    r = api.request('statuses/user_timeline',
+                    {'screen_name':screen_name, 'count':200})
+    tweetCount = 1
+    for item in r.get_iterator():
+        if 'text' in item.keys():
+            print str(tweetCount) + ': ' + item['text']
+            tweetCount += 1
+
+def collect_stream(api):
+    r = api.request('statuses/sample')
+    tweetCount = 0
+    for item in r.get_iterator():
+        if 'text' in item.keys():
+            tweet = item['text']
+            screen_name = item['user']['screen_name']
+            # Here we need to check whether a tweet is Chinese or Japanese
+            if (any(u'\u4e00' <= ch <= u'\u9fff' for ch in tweet)
+                and not any(u'\u3040' <= ch <= u'\u30ff' for ch in tweet)):
+                print screen_name
+                print tweet
+                analyze_user(api, screen_name)
+
+            tweetCount += 1
+            if tweetCount % 1000 == 0:
+                print tweetCount
+
+
 api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET,
                  ACCESS_KEY, ACCESS_SECRET)
 
-# Note: At least one predicate parameter (follow, locations, or track)
-# must be specified.
-r = api.request('statuses/sample')
-tweetCount = 0
-for item in r.get_iterator():
-    if 'text' in item.keys():
-        tweet = item['text']
-        screen_name = item['user']['screen_name']
-        # Here we need to check whether a tweet is Chinese or Japanese
-        if (any(u'\u4e00' <= ch <= u'\u9fff' for ch in tweet)
-            and not any(u'\u3040' <= ch <= u'\u30ff' for ch in tweet)):
-            print screen_name
-            print tweet
-
-            rr = api.request('statuses/user_timeline',
-                             {'screen_name':screen_name, 'count':200})
-            count = 1
-            for i in rr.get_iterator():
-                if 'text' in i.keys():
-                    print str(count) + ': ' + i['text']
-                    count += 1
-            
-        tweetCount += 1
-        if tweetCount % 1000 == 0:
-            print tweetCount
+collect_stream(api)
