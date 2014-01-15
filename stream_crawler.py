4f504bab1f59cf8afa21cde465838fd72150fd85
SudoAttack <wayne8798@gmail.com>
2014-01-14 22:31:52 -0600

    now sort of filter out @'s and urls

--- stream_crawler.py
+++ stream_crawler.py
@@ -1,4 +1,5 @@
 from TwitterAPI import TwitterAPI
+from itertools import chain
 
 # the following credentials can be found in the app page in tchpaloalto
 CONSUMER_KEY = "Oyebr7Dx5LVseGP5tsuA"
@@ -10,11 +11,31 @@ ACCESS_SECRET = "FxbhHZ5g5ddLdGFGI4TTTlwuScFoOIVUi7FZSQBxqg4DD"
 def analyze_user(api, screen_name):
     r = api.request('statuses/user_timeline',
                     {'screen_name':screen_name, 'count':200})
-    tweetCount = 1
+    chCount = 1
+    enCount = 1
+    japFlag = False
     for item in r.get_iterator():
         if 'text' in item.keys():
-            print str(tweetCount) + ': ' + item['text']
-            tweetCount += 1
+            text = item['text']
+            entities = item['entities']
+            urls = (e["url"] for e in entities["urls"])
+            users = ("@"+e["screen_name"] for e in entities["user_mentions"])
+            text = reduce(lambda t,s: t.replace(s, ""), chain(urls, users), text)
+            
+            for ch in text:
+                if u'\u3040' <= ch <= u'\u30ff':
+                    japFlag = True
+                    break
+
+                if u'\u4e00' <= ch <= u'\u9fff':
+                    chCount += 1
+                elif 'A' <= ch <= 'z':
+                    enCount += 1
+        if japFlag:
+            break
+
+    if chCount > 50 and enCount > 200 and not japFlag:
+        print screen_name
 
 def collect_stream(api):
     r = api.request('statuses/sample')
@@ -26,8 +47,6 @@ def collect_stream(api):
             # Here we need to check whether a tweet is Chinese or Japanese
             if (any(u'\u4e00' <= ch <= u'\u9fff' for ch in tweet)
                 and not any(u'\u3040' <= ch <= u'\u30ff' for ch in tweet)):
-                print screen_name
-                print tweet
                 analyze_user(api, screen_name)
 
             tweetCount += 1
